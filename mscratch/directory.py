5e31d73186a58a63d1a18506ea535f61fff81a3e
Yovan Naumovski <naumovski.jovan@gmail.com>
2016-04-08 13:18:05 +0300

    'url' and/or 'content' string can be passed extract_links in DirectoryLinkExtractor, instead of just passing url to __init__. The http code should be isolated from the DirectoryLinkExtractor class.

--- mscratch/directory.py
+++ mscratch/directory.py
@@ -4,20 +4,27 @@ from lxml import html
 class DirectoryLinkExtractor:
     ''' Extracts links of pages that contain data content. '''
 
-    def __init__(self, url):
-        self.url = url
+    @staticmethod
+    def request_page(url):
+        return requests.get(url).content
 
-    def get_page_tree(self):
-        html_page = requests.get(self.url)
-        return html.fromstring(html_page.content)
+    def get_page_tree(self, page_string):
+        return html.fromstring(page_string)
 
-    def extract_links(self, xpath, base_url_append=True):
+    def extract_links(self, xpath, base_url_append=True, url=None, content=None):
         ''' Returns list of href attribute values of elements described with an XPath. '''
+        if not url and not content:
+            raise ValueError("Both url and content are None. At least one should have value other than None.")
 
-        tree = self.get_page_tree()
+        if content:
+            tree = self.get_page_tree(content)
+        else:
+            page_string = DirectoryLinkExtractor.request_page(url)
+            tree = self.get_page_tree(page_string)
+
         links = tree.xpath(xpath)
 
         if base_url_append:
-            return [(self.url + link.attrib['href']) for link in links]
+            return [(url + link.attrib['href']) for link in links]
 
         return [link.attrib['href'] for link in links]
