cc8f9d26eee2ed0006612dead019237e2fdf83d1
Yovan Naumovski <jovan3.14@gmail.com>
2017-08-05 20:45:45 +0300

    add post method to HttpClient

--- mscratch/httpclient.py
+++ mscratch/httpclient.py
@@ -17,12 +17,18 @@ class HttpClient:
         resp = requests.get(url, headers=self.params.headers, timeout=self.params.timeout)
         return resp.content
 
+    def post(self, url, data=None):
+        resp = requests.post(url, headers=self.params.headers, timeout=self.params.timeout, data=data)
+        return resp.content
+    
 class HttpClientParams:
     ''' Class holding parameter values used by requests http clients. '''
 
     DEFAULT_UA = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv: 45.0) Gecko/20100101 Firefox/45.0'}
 
-    def __init__(self, headers=None, timeout=40):
+    DEFAULT_TIMEOUT = 40
+
+    def __init__(self, headers=None, timeout=DEFAULT_TIMEOUT):
         '''
         Parameters:
             headers - <dict> HTTP request headers
--- mscratch/test/test_httpclient.py
+++ mscratch/test/test_httpclient.py
@@ -1,16 +1,49 @@
 from mscratch.httpclient import HttpClient, HttpClientParams
+from unittest.mock import Mock
 import pytest
 import requests
 
+class ResponseMock:
+
+    def __init__(self):
+        self.content = 'mock_content'
+
+        
 @pytest.fixture
 def client():
     params = HttpClientParams(headers=HttpClientParams.DEFAULT_UA)
     return HttpClient(params)
 
+
 def test_httpclient_get_empty_url(client):
     with pytest.raises(requests.exceptions.MissingSchema):
         client.get(None)
-
+        
+        
 def test_httpclient_get_invalid_url(client):
     with pytest.raises(requests.exceptions.MissingSchema):
         client.get('a.b.c')
+
+        
+def test_httpclient_post():
+    params = HttpClientParams(headers={'foo': 'bar'}, timeout=10)
+    client = HttpClient(params)
+    requests.post = Mock(return_value=ResponseMock())
+
+    response_content = client.post('http://example.org')
+    requests.post.assert_called_with('http://example.org',
+                                     headers={'foo': 'bar'},
+                                     timeout=10,
+                                     data=None)
+    
+    assert response_content == 'mock_content'
+
+def test_httpclient_post_with_post_data(client):
+    requests.post = Mock()
+    
+    client.post('http://example.org', data='payload')
+    requests.post.assert_called_with('http://example.org',
+                                     headers=HttpClientParams.DEFAULT_UA,
+                                     timeout=HttpClientParams.DEFAULT_TIMEOUT,
+                                     data='payload')
+
